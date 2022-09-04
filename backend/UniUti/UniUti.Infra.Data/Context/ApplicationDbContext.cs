e4660a729f3ba2e07559ebed29453933e2ea0531
David Santos <gft.davids@grupofleury.com.br>
2022-09-04 17:49:45 -0300

    bug contexto

--- backend/UniUti/UniUti.Infra.Data/Context/ApplicationDbContext.cs
+++ backend/UniUti/UniUti.Infra.Data/Context/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace UniUti.Database
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Instituicao> Instituicoes { get; set; }
         public DbSet<Monitoria> Monitorias { get; set; }
-
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
 
